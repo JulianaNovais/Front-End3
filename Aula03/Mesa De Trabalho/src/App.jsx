92768e36dd3d27c52c1730d32d3c0504d5362a3e
Juliana Novais <juliana.novais@icloud.com>
2023-03-11 16:57:24 -0300

    Projeto realizado na mesa de trabalho

---
+++ Aula03/Mesa De Trabalho/src/App.jsx
@@ -0,0 +1,12 @@
+import Componente from "./Componente";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Componente />
+    </div>
+  );
+}
+
+export default App;
---
+++ Aula03/Mesa De Trabalho/src/Componente.jsx
@@ -0,0 +1,9 @@
+function Componente() {
+  return (
+    <div>
+      <h1 style={{ color: "pink" }}>Olá mundo!</h1>
+    </div>
+  );
+}
+
+export default Componente;
